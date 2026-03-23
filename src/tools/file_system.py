e8c2476df57b4ff7336cdb22be4fed11cfbbdf4f
SamudaEdiriweera <samudaediriweera@gmail.com>
2026-03-23 18:44:50 +0530

    use @tool decorator instead manual tool calling

--- src/tools/file_system.py
+++ src/tools/file_system.py
@@ -9,33 +9,52 @@ it must be able to:
 
 import os 
 import mlflow
+from langchain_core.tools import tool
+from src.utils.tracker import AgentTracker
+
 
 class FileSystemTool:
-    def __init__(self, base_path="workspace"):
+    def __init__(self, MLflow_tracker: AgentTracker, base_path="workspace"):
         self.base_path = base_path
+        self.MLflow_tracker = MLflow_tracker # USe the centralized tracker service
         # Ensure the workspace exists
         if not os.path.exists(self.base_path):
             os.makedirs(self.base_path)
-
+    
+    # We use the @tool decorator here
+    # IMPORTANT: The docstring below is what the AI reads to understand the tool!
+    @tool
     def write_file(self, file_path: str, content: str):
-        """ Write code to a specific file in the workspace."""
-        full_path = os.path.join(self.base_path, file_path) # Combine base path with the provided file path
+        """ 
+        Write code to a specific file in the workspace.
+        USe this tool to  create Reat components, package.json, or ML logic files.
+        Arguments:
+        - file_path: The relative path including filename (e.g., 'src/components/Button.tsx)
+        - content: The actual code string to write.
+        """
 
+        full_path = os.path.join(self.base_path, file_path) # Combine base path with the provided file path
         # Create directories if they don't exist
         os.makedirs(os.path.dirname(full_path), exist_ok=True) # Create the directory structure if it doesn't exist
 
         with open(full_path, "w") as f: # Open the file for writing
             f.write(content)
 
-        # INDUSTRY STANDARD: Log this 'action' to MLflow for traceability
-        mlflow.log_text(content, f"genreated_files/{file_path}") # Log the generated file content to MLflow for traceability
+        # USE TRACKER SERVICE
+        # instead of calling mlflow directly, we use our standardized service.
+        # This ensures the artifacts are saved in the 'production_ready' folder.
+        self.MLflow_tracker.log_final_output({file_path: content})
+
         print(f"📂 Created file: {full_path}")
         return f"Successfully wrote to {file_path}"
     
+    @tool
     def list_files(self):
-        """ Allows the AI to see what it has built so far"""
+        """ Returns a list of all files currently in the workspace.
+            USe this to see what you have already exist.
+        """
         files_list = [] # Initialize an empty list to store file paths
-        for root, dirs, files  in os.walk(self.base_path): # Walk through the base directory and its subdirectories
+        for root, _, files  in os.walk(self.base_path): # Walk through the base directory and its subdirectories
             for file in files: # Iterate through each file found
                 files.append(os.path.relpath(os.path.join(root, file), self.base_path)) # Append the relative path of the file to the files_list
         return files_list # Return the list of file paths
